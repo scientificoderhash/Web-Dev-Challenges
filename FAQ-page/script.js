0b0f4bf3f05507fb1574e5aea285f95aef172848
scientificoderhash <freelancergpt123@gmail.com>
2025-01-26 15:46:49 +0530

    FAQ-challenge has been added as well

---
+++ FAQ-page/script.js
@@ -0,0 +1,17 @@
+// document.body.addEventListener("click", ()=>{
+//     document.body.querySelector(".ans-1").style.display = "block"
+// })
+
+document.body.childNodes[3].childNodes[1].childNodes[3].childNodes[1].addEventListener("click", ()=>{
+    document.body.querySelector(".ans-1").style.display = "block"
+})
+document.body.childNodes[3].childNodes[1].childNodes[3].childNodes[3].addEventListener("click", ()=>{
+    document.body.querySelector(".ans-2").style.display = "block"
+})
+document.body.childNodes[3].childNodes[1].childNodes[3].childNodes[5].addEventListener("click", ()=>{
+    document.body.querySelector(".ans-3").style.display = "block"
+})
+document.body.childNodes[3].childNodes[1].childNodes[3].childNodes[7].addEventListener("click", ()=>{
+    document.body.querySelector(".ans-4").style.display = "block"
+})
+
