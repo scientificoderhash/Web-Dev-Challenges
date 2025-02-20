18bbf09aad7b296a69fdd43f3abea4419060f4bf
scientificoderhash <freelancergpt123@gmail.com>
2025-02-20 23:54:01 +0530

    sundown studio is conmplete, just need to add swiper js

--- sundown-studio/script.js
+++ sundown-studio/script.js
@@ -1,7 +1,7 @@
-// const scroll = new LocomotiveScroll({
-//     el: document.querySelector('#main'),
-//     smooth: true
-// });
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
 
 function page4Animation() {
     var elemC = document.querySelector("#elem-container")
@@ -30,5 +30,33 @@ function swiperAnimation() {
     });
 }
 
+function menuAnimation() {
+
+    var menu = document.querySelector("nav h3")
+    var full = document.querySelector("#full-scr")
+    var navimg = document.querySelector("nav img")
+    var flag = 0
+    menu.addEventListener("click", function () {
+        if (flag == 0) {
+            full.style.top = 0
+            navimg.style.opacity = 0
+            flag = 1
+        } else {
+            full.style.top = "-100%"
+            navimg.style.opacity = 1
+            flag = 0
+        }
+    })
+}
+
+function loaderAnimation() {
+    var loader = document.querySelector("#loader")
+    setTimeout(function () {
+        loader.style.top = "-100%"
+    }, 4200)
+}
+
+loaderAnimation();
+menuAnimation();
 page4Animation();
 swiperAnimation();
