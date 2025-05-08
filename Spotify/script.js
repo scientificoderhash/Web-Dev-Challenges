1b9836c85a88795ec16076a689bc38a902680d1d
scientificoderhash <freelancergpt123@gmail.com>
2025-05-08 17:59:44 +0530

    To-Do-List mvp complete

--- Spotify/script.js
+++ Spotify/script.js
@@ -1,7 +1,7 @@
 console.log("Hello World!")
 
 async function main(){
-let a = await fetch("C:/Users/admn/Desktop/Challenges/Spotify/songs/")
+let a = await fetch("E:\previous files\kirtan-20241215T162424Z-001\kirtan")
 let response = await a.text();
 console.log(response)
 }
